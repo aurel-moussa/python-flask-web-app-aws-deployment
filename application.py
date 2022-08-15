1113c3807f320d0a60360572ce0ba9a33cbf8c72
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2022-08-15 15:51:54 +0200

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,13 @@
-from flask import Flask
-application = Flask(__name__)
+#Run this on your console if you havent got Flask yet
+#pip3 install flask
 
-@application.route('/')
+from flask import Flask #imports Flask library
+#needs Python 2.6 or higher
+application = Flask(__name__) #instanting the library
+
+@application.route('/') #tells URL which URL should call the associated function
 def hello_world():
     return "Hello World"
 
-if __name__ == '__main__':
+if __name__ == '__main__': #runs this application
     application.run()
