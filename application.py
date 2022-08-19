591d4b91b12c8e0d67c23367bc5416ca5a8e9f4f
aurel-moussa <62769836+aurel-moussa@users.noreply.github.com>
2022-08-19 12:19:34 +0200

    Update application.py

--- application.py
+++ application.py
@@ -3,10 +3,13 @@
 
 from flask import Flask #imports Flask library
 #needs Python 2.6 or higher
+import os #needed to access global variables stored in Amazon AWS
+
 application = Flask(__name__) #instanting the library
 
 @application.route('/') #tells URL which URL should call the associated function
 def hello_world():
+    db_name = os.environ['DATABASE_NAME'] #the environment variable key-value pair was created in Configurations > Software in AWS
     return "Hello World"
 
 if __name__ == '__main__': #runs this application
